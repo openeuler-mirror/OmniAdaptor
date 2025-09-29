a170738901807c28318a9d9039486c84868961e4
helloxteen <helloxteen@foxmail.com>
2025-09-29 19:32:51 +0800

    update License

--- omnioperator/omniop-spark-extension/spark-extension-shims/spark35-modify/src/main/scala/org/apache/spark/sql/execution/datasources/OmniFileFormatDataWriter.scala
+++ omnioperator/omniop-spark-extension/spark-extension-shims/spark35-modify/src/main/scala/org/apache/spark/sql/execution/datasources/OmniFileFormatDataWriter.scala
@@ -1,5 +1,4 @@
 /*
- * Copyright (C) 2024-2024. Huawei Technologies Co., Ltd. All rights reserved.
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
