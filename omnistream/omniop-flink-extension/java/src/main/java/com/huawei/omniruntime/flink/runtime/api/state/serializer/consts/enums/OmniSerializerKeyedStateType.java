f513efcfeae9b4b1dd01b0aa06143a59d1556e3d
TideMacro <TideMacro@outlook.com>
2026-08-13 16:12:21 +0800

    fix serializer about

--- omnistream/omniop-flink-extension/java/src/main/java/com/huawei/omniruntime/flink/runtime/api/state/serializer/consts/enums/OmniSerializerKeyedStateType.java
+++ omnistream/omniop-flink-extension/java/src/main/java/com/huawei/omniruntime/flink/runtime/api/state/serializer/consts/enums/OmniSerializerKeyedStateType.java
@@ -8,20 +8,22 @@ import org.apache.flink.api.common.state.StateDescriptor;
  */
 
 public enum OmniSerializerKeyedStateType {
-    UNKNOWN("0", StateDescriptor.Type.UNKNOWN),
-    VALUE("1", StateDescriptor.Type.VALUE),
-    LIST("2", StateDescriptor.Type.LIST),
-    REDUCING("3", StateDescriptor.Type.REDUCING),
-    FOLDING("4", StateDescriptor.Type.FOLDING),
-    AGGREGATING("5", StateDescriptor.Type.AGGREGATING),
-    MAP("6", StateDescriptor.Type.MAP),
+    UNKNOWN("0", "UNKNOWN", StateDescriptor.Type.UNKNOWN),
+    VALUE("1", "VALUE", StateDescriptor.Type.VALUE),
+    LIST("2", "LIST", StateDescriptor.Type.LIST),
+    REDUCING("3", "REDUCING", StateDescriptor.Type.REDUCING),
+    FOLDING("4", "FOLDING", StateDescriptor.Type.FOLDING),
+    AGGREGATING("5", "AGGREGATING", StateDescriptor.Type.AGGREGATING),
+    MAP("6", "MAP", StateDescriptor.Type.MAP),
     ;
 
     private final String code;
+    private final String name;
     private final StateDescriptor.Type type;
 
-    OmniSerializerKeyedStateType(String code, StateDescriptor.Type type) {
+    OmniSerializerKeyedStateType(String code, String name, StateDescriptor.Type type) {
         this.code = code;
+        this.name = name;
         this.type = type;
     }
 
@@ -29,20 +31,24 @@ public enum OmniSerializerKeyedStateType {
         return this.type;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
     public String getTypeName() {
         return this.type.name();
     }
 
-    public boolean equals(String code) {
-        return this.code.equalsIgnoreCase(code);
+    public boolean equals(String str) {
+        return this.code.equalsIgnoreCase(str) || this.name.equalsIgnoreCase(str);
     }
 
-    public static OmniSerializerKeyedStateType get(String code) {
-        if(null == code){
+    public static OmniSerializerKeyedStateType get(String str) {
+        if(null == str){
             return null;
         }
         for (OmniSerializerKeyedStateType item : OmniSerializerKeyedStateType.values()) {
-            if (item.equals(code)) {
+            if (item.equals(str)) {
                 return item;
             }
         }
